a1954369d559583ede4eb1a5bf565cea22b70e49
myosubug <seanoh.dev@gmail.com>
2018-10-02 23:14:38 -0600

    data updated by top 20 Canadian cities by population

--- webmap.py
+++ webmap.py
@@ -1,20 +1,27 @@
 import folium
 import pandas
 
-data = pandas.read_csv("Volcanoes.txt")
-lat = list(data["LAT"])
-lon = list(data["LON"])
-elev = list(data["ELEV"])
+data = pandas.read_csv("ca_updated.csv")
+lat = list(data["lat"])
+lon = list(data["lng"])
+name = list(data["city"])
+pop = list(data["population"])
 
-map = folium.Map(location=[51.04, -114.062], zoom_start=4)
+def color_generator(ppl):
+    if ppl < 150000:
+        return 'green'
+    elif 150000 <= ppl < 750000:
+        return 'orange'
+    else:
+        return 'red'
+    return
+
+map = folium.Map(location=[51.04, -114.062], zoom_start=3)
 
 fg = folium.FeatureGroup(name="Base")
 
-fg.add_child(folium.Marker(location=[51.04, -114.062], popup="Hi! this is snowing Calgary!", icon=folium.Icon(color='red')))
-for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup=el, icon=folium.Icon(color='orange')))
-
+for lt, ln, pp, nm in zip(lat, lon, pop, name):
+    fg.add_child(folium.CircleMarker(location=[lt,ln], popup=nm, color='grey', radius=3, fill_color=color_generator(pp), fill_opacity=1))
 
 map.add_child(fg)
-
 map.save("webmap.html")
